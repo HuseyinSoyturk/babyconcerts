0e34e81be9e899bdfbed8b3269d9fa2010f2388c
huseyinsoyturk <soyturk.huseyin61@gmail.com>
2023-02-05 23:25:35 +0300

    hasancan eklendi

--- index.js
+++ index.js
@@ -1,15 +1,73 @@
 const { default: axios } = require("axios");
-var cron = require('node-cron');
+const cron = require('node-cron');
+const nodemailer = require('nodemailer');
 
 async function name() {
-    const body = { "q": "Babyconcerts", "filter": ["locale='tr'", "eventEndDateTimestamp >= 1675619701.694"], "attributesToRetrieve": ["eventName", "eventId", "isParent"], "offset": 0, "sort": ["eventStartDateTimestamp:asc"], "limit": 7 }
-    const config = {
-        headers: { Authorization: `Bearer a63d121547b0d950de3df57bba212d74a37a45973ee4cb07038f1ba8bc1bcd7d` }
-    };
-    const response = await axios.post('https://search.mobilet.com/indexes/event/search', body, config)
-    console.log(response.data.hits);
+    try {
+        const response = await axios('https://mobilet.com/_next/data/zknB8r_WcCZzZ_o2xhN1c/tr/search.json?text=Babyconcerts')
+        if (response.data.pageProps && response.data.pageProps.searchResult && response.data.pageProps.searchResult.eventCards) {
+            const eventCards = response.data.pageProps.searchResult.eventCards
+            const condition = eventCards.some(obj => obj.href !== '/event/17453')
+            if (condition) {
+                var transporter = nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                        user: 'soyturk.huseyin61@gmail.com',
+                        pass: 'gufdchiagjpxifcz'
+                    }
+                });
+
+                var mailOptions = {
+                    from: 'soyturk.huseyin61@gmail.com',
+                    to: 'soyturk.huseyin61@gmail.com',
+                    subject: 'BabyConcerts Onemli !!!!',
+                    text: JSON.stringify(response.data.hits)
+                };
+
+                transporter.sendMail(mailOptions, function (error, info) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                    }
+                });
+            }
+        }
+
+        const response2 = await axios('https://mobilet.com/_next/data/zknB8r_WcCZzZ_o2xhN1c/tr/search.json?text=Konuşanlar - Hasan Can Kaya')
+        if (response2.data.pageProps && response2.data.pageProps.searchResult && response2.data.pageProps.searchResult.eventCards) {
+            const eventCards = response2.data.pageProps.searchResult.eventCards
+            const condition = eventCards.length > 0
+            if (condition) {
+                var transporter = nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                        user: 'soyturk.huseyin61@gmail.com',
+                        pass: 'gufdchiagjpxifcz'
+                    }
+                });
+
+                var mailOptions = {
+                    from: 'soyturk.huseyin61@gmail.com',
+                    to: 'soyturk.huseyin61@gmail.com',
+                    subject: 'Konuşanlar Onemli !!!!',
+                    text: JSON.stringify(response.data.hits)
+                };
+
+                transporter.sendMail(mailOptions, function (error, info) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                    }
+                });
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-cron.schedule('*/1 * * * *', () => {
+cron.schedule('*/5 * * * *', () => {
     name();
 });
